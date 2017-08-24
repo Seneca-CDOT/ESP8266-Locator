2b6b36cf7d4df423d5a6e755d7527ab6210ffe3d
Samantha Yao <yaosa@utschools.ca>
2017-08-24 13:21:36 -0400

    successful rest?

--- HTTPClient/HTTPClient.ino
+++ HTTPClient/HTTPClient.ino
@@ -1,43 +1,143 @@
-#include <ESP8266HTTPClient.h>
+/*
+  This a simple example of the aREST Library for the ESP8266 WiFi chip.
+  See the README file for more details.
+
+  Written in 2015 by Marco Schwartz under a GPL license.
+*/
+
+// Import required libraries
 #include <ESP8266WiFi.h>
- 
-void setup() {
- 
-  Serial.begin(115200);                                  //Serial connection
-  WiFi.begin("ESP8266-Dragon", "DragonFruit");   //WiFi connection
- 
-  while (WiFi.status() != WL_CONNECTED) {  //Wait for the WiFI connection completion
- 
+#include <aREST.h>
+
+// Create aREST instance
+aREST rest = aREST();
+
+// WiFi parameters
+const char* ssid = "ESP8266-Dragon";
+const char* password = "DragonFruit";
+
+// The port to listen for incoming TCP connections
+#define LISTEN_PORT           80
+
+// Create an instance of the server
+WiFiServer server(LISTEN_PORT);
+
+// Variables to be exposed to the API
+int temperature;
+int humidity;
+int timestamp;
+String mac;
+String essid;
+String strength;
+String bssid;
+
+// Declare functions to be exposed to the API
+void getData();
+
+void setup(void)
+{
+  // Start Serial
+  Serial.begin(115200);
+
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
     delay(500);
-    Serial.println("Waiting for connection");
- 
+    Serial.print(".");
   }
- 
+  Serial.println("");
+  Serial.println("WiFi connected");
+
+  // Start the server
+  server.begin();
+  Serial.println("Server started");
+
+  // Print the IP address
+  Serial.println(WiFi.localIP());
+  
 }
- 
+
 void loop() {
- 
- if(WiFi.status()== WL_CONNECTED){   //Check WiFi connection status
- 
-   HTTPClient http;    //Declare object of class HTTPClient
- 
-   http.begin("http://192.168.4.1/led/");      //Specify request destination
-   http.addHeader("Content-Type", "text/plain");  //Specify content-type header
- 
-   int httpCode = http.POST("Message from ESP8266");   //Send the request
-   String payload = http.getString();                  //Get the response payload
- 
-   Serial.println(httpCode);   //Print HTTP return code
-   Serial.println(payload);    //Print request response payload
- 
-   http.end();  //Close connection
- 
- }else{
- 
-    Serial.println("Error in WiFi connection");   
- 
- }
- 
-  delay(30000);  //Send a request every 30 seconds
- 
+  getData();
+
+  // Handle REST calls
+  WiFiClient client = server.available();
+  if (!client) {
+    return;
+  }
+  while(!client.available()){
+    delay(1);
+  }
+  rest.handle(client);
+
+  delay(60*10000);
+
 }
+
+void getData(){
+  //Disconnect to scan
+  WiFi.mode(WIFI_STA);
+  WiFi.disconnect();
+  delay(100);
+  //scans for networks
+  int n = WiFi.scanNetworks(); //n = number of networks found when scanning
+  if (n == 0)
+    Serial.println("no networks found");
+  else{
+    // Init variables and expose them to REST API
+    timestamp = millis();
+    mac = WiFi.macAddress();
+    essid = "";
+    for (int i = 0; i < n; ++i)
+      {
+        essid = essid + WiFi.SSID(i);
+        if (i != n-1){
+          essid = essid + ",";
+        }   
+      }
+    strength = "";
+    for (int i = 0; i < n; ++i)
+      {
+        strength = strength + String(WiFi.RSSI(i));
+        if (i != n-1){
+          strength = strength + ",";
+        }   
+      }
+    bssid = "";
+    for (int i = 0; i < n; ++i)
+      {
+        bssid = bssid + WiFi.BSSIDstr(i);
+        if (i != n-1){
+          bssid = bssid + ",";
+        }   
+      }
+    rest.variable("timestamp",&timestamp);
+    rest.variable("ESSID",&essid);
+    rest.variable("Signal",&strength);
+    rest.variable("BSSID",&bssid);
+    
+    // Function to be exposed
+  
+    // Give name & ID to the device (ID should be 6 characters long)
+    rest.set_id(mac);
+    rest.set_name("esp8266");
+
+    //Connects  to WiFi
+    WiFi.begin(ssid, password);
+    while (WiFi.status() != WL_CONNECTED) {
+      delay(500);
+      Serial.print(".");
+    }
+    Serial.println("");
+    Serial.println("WiFi connected");
+  
+    // Start the server
+    server.begin();
+    Serial.println("Server started");
+  
+    // Print the IP address
+    Serial.println(WiFi.localIP());
+
+    delay(60*10000);
+  }
+}
+
