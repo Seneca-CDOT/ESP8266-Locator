6e3cfa66f01abad471555c29fd8e55cad9ab6c16
Ethan Childerhose <ethanchilderhose@gmail.com>
2017-09-01 15:31:04 -0400

    Final Version Code

--- Serial/Serial.ino
+++ Serial/Serial.ino
@@ -4,16 +4,11 @@ void setup() {
   // put your setup code here, to run once:
   Serial.begin(115200);
 
-  pinMode(13, INPUT_PULLUP);
-  pinMode(12, INPUT_PULLUP);
-  pinMode(14, INPUT_PULLUP);
 
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  if(digitalRead(13) == LOW) Serial.println(":(");
-  if(digitalRead(12) == LOW) Serial.println(":|");
-  if(digitalRead(14) == LOW) Serial.println(":)");
+  Serial.println(analogRead(0));
   delay(20);
 }
